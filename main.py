ca21ba6433b28b8303098f0ed9e3d8ecc3a3356c
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2024-05-29 18:49:59 +0200

    Add Window size

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
+options.add_argument("--window-size=1280,700")
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
