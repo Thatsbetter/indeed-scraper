bf2ea35ffa72228d7785724ffbbce61cb7c0fc1a
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2024-05-29 18:50:50 +0200

    Adjust headless argument

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
 options = webdriver.ChromeOptions()
-options.add_argument('--headless')
+options.add_argument("--headless=new")
 options.add_argument("--window-size=1280,700")
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
