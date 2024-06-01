62dda6b34ac5133f56011e5eb56ba8a8b92d98f3
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2024-06-01 17:54:08 +0200

    Add profiles to uc

--- uc-test.py
+++ uc-test.py
@@ -1,7 +1,20 @@
 import undetected_chromedriver as uc
+import time
+options = uc.ChromeOptions()
+options.headless = True
+browser = uc.Chrome(options=options)
 
-driver = uc.Chrome()
-driver.get('https://indeed.com')
-print(driver.title)
-driver.close()
-driver.quit()
+from selenium_profiles.webdriver import profiles as profile_manager
+from selenium_profiles.profiles import profiles
+
+profile = profiles.Android()  # or .Android()
+browser.profiles = profile_manager(driver=browser, profile=profile)
+browser.profiles.apply(profile)
+time.sleep(5)
+browser.minimize_window()
+browser.get('https://indeed.com')
+browser.maximize_window()
+browser.save_screenshot("indeed.png")
+print(browser.title)
+browser.close()
+browser.quit()
