2da32627396fba0efaea4097ab3c792f7dccd97b
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2024-06-06 08:06:03 +0200

    Refactor main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import time
 
 from selenium import webdriver
@@ -6,64 +7,76 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+
 from proxy import Proxy
-import random
+
 
 def does_it_connect_to_indeed(browser):
-    browser.get("https://de.indeed.com/?r=us")
-    if "indeed" in browser.title.lower():
+    browser.get("http://de.indeed.com/?r=us")
+    if "job" in browser.title.lower():
         return True
-    browser.quit()
-    return False
+    else:
+        return False
 
-def initialize_browser():
-    browser = ""
-    p = Proxy().get_proxies_by_protocol('http')[:100]
-    connected_proxies = []
-    for proxy in p:
-        if Proxy.is_proxy_working(f"{proxy['ip']}:{proxy['port']}"):
-            connected_proxies.append(proxy)
-    print(len(connected_proxies))
+
+def validate_proxy(proxy_link):
+    """Check if the proxy works for Indeed."""
+    try:
+        with initialize_browser(proxy_link) as browser:
+            browser.set_page_load_timeout(5)
+            return does_it_connect_to_indeed(browser)
+    except:
+        return False
+
+
+def get_indeed_proxies():
+    indeed_proxies = []
+    connected_proxies = Proxy().get_http_and_false_ssl()[:25]
+    for connect in connected_proxies:
+        proxy_link = f"{connect['ip']}:{connect['port']}"
+        if validate_proxy(proxy_link):
+            indeed_proxies.append(proxy_link)
+        if len(indeed_proxies) > 5:
+            break
+
+    if indeed_proxies:
+        return indeed_proxies
+    else:
+        print("No proxies found.")
+        time.sleep(400)
+        return get_indeed_proxies()
+
+
+def initialize_browser(proxy_link):
     options = webdriver.ChromeOptions()
     options.add_argument("--headless=new")
+    options.add_argument('--proxy-server=http://{}'.format(proxy_link))
     options.add_argument("--window-size=1280,700")
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
     options.add_argument(
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
-    for conect in connected_proxies:
-        try:
-            proxy_link = f"{conect['ip']}:{conect['port']}"
-            options.add_argument('--proxy-server=https://{}'.format(proxy_link))
-            res = does_it_connect_to_indeed(webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options))
-            if res:
-                return webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-        except:
-            continue
-    raise Exception("No working proxy found")
+    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+    return browser
 
-def get_job_links(skills, place, browser, num_pages=5):
+
+def get_job_links(skill, place, browser, page=1):
     job_links = []
-    base_url = "https://de.indeed.com/jobs?q={}&l={}&fromage=14&start={}"
-
-    for job_keyword in skills:
-        for page in range(num_pages):
-            url = base_url.format(job_keyword.replace(' ', '%20'), place, page * 10)
-            browser.get(url)
-            try:
-                WebDriverWait(browser, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'tapItem')))
-                jobs = browser.find_elements(By.CLASS_NAME, 'tapItem')
-
-                for job in jobs:
-                    try:
-                        job_link_element = job.find_element(By.CSS_SELECTOR, 'a.jcs-JobTitle')
-                        job_link = job_link_element.get_attribute('href')
-                        job_links.append(job_link)
-                    except Exception as e:
-                        print(f"Error processing job: {e}")
-            except Exception as e:
-                print(f"Error loading page: {e}")
+    skill = skill.replace(' ', '+')
+    url = f"http://de.indeed.com/jobs?q={skill}&l={place}&fromage=14&start={page * 10}"
+    try:
+        browser.get(url)
+        time.sleep(5)
+        browser.save_screenshot(f"indeed_{page}.png")
+        # WebDriverWait(browser, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'tapItem')))
+        jobs = browser.find_elements(By.CLASS_NAME, 'tapItem')
+        for job in jobs:
+            job_link_element = job.find_element(By.CSS_SELECTOR, 'a.jcs-JobTitle')
+            job_link = job_link_element.get_attribute('href')
+            job_links.append(job_link)
+    except Exception as e:
+        print(f"Error loading page: {str(e)}")
     return job_links
 
 
@@ -126,16 +139,19 @@ def main():
     ]
     places = ["hamburg", "deutschland"]
 
-    browser = initialize_browser()
-    try:
-        for skill in skills:
-            for place in places:
-                time.sleep(5)
-                job_links = get_job_links(skill, place, browser)
+    proxy_list = get_indeed_proxies()
+    print(f"Found {len(proxy_list)} working proxies.")
+    job_links = []
+    for skill in skills:
+        for place in places:
+            time.sleep(5)
+            for page in range(1, 5):
+                with initialize_browser(random.choice(proxy_list)) as browser:
+                    job_links = get_job_links(skill, place, browser, page)
                 print(f"Found {len(job_links)} job links.")
-                scrape_job_details(browser, job_links, home_office_keyword, job_requirements_keyword)
-    finally:
-        browser.quit()
+    for job_link in job_links:
+        with initialize_browser(random.choice(proxy_list)) as browser:
+            scrape_job_details(browser, job_link, home_office_keyword, job_requirements_keyword)
 
 
 if __name__ == "__main__":
